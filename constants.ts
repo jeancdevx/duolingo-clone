f24b35a4a64db1f19c1d6a585cbe04eef654238c
jcodev2 <jcode2006@gmail.com>
2024-09-24 18:59:51 -0500

    feat: :sparkles: add constants file

---
+++ constants.ts
@@ -0,0 +1,28 @@
+export const POINTS_TO_REFILL_HEARTS = 10
+export const MAX_HEARTS = 5
+export const MIN_HEARTS = 0
+export const IDENTATION_PX = 45
+export const LIMIT_LEADERBOARD_USERS = 10
+
+export const quests = [
+  {
+    title: 'Earn 20XP',
+    value: 20
+  },
+  {
+    title: 'Earn 50XP',
+    value: 50
+  },
+  {
+    title: 'Earn 100XP',
+    value: 100
+  },
+  {
+    title: 'Earn 500XP',
+    value: 500
+  },
+  {
+    title: 'Earn 1000XP',
+    value: 1000
+  }
+]
